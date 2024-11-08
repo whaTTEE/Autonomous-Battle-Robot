62abedf465c4e5c60878a0aa0a1ce13c543b443e
whaTTEE <154565464+whaTTEE@users.noreply.github.com>
2024-11-09 03:39:45 +0900

    Update UltrasonicSensor.cpp

--- UltrasonicSensor.cpp
+++ UltrasonicSensor.cpp
@@ -1,4 +1,6 @@
-//
+#include <NewPing.h> 초음파센서 구동위한 라이브러리, 
+//라이브러리 설치: 아두이노 IDE에서 Sketch > Include Library > Manage Libraries...로 가서 NewPing을 검색하여 설치
+
 int time = 0;
 
 int input = 0;
@@ -7,9 +9,9 @@ int distance = 0;
 
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
+  pinMode(triggerPin, OUTPUT);  //트리거핀이 신호를 보내기 위해 설정
+  digitalWrite(triggerPin, LOW); //전압이 0인 상태로 트리거핀을 초기화, 안정된 상태에서 신호를 받기 시작함
+  delayMicroseconds(2); //아두이노 2마이크로초 동안 아무 작업 없이 대기. 신호의 안정성 높이기 위함
   // Sets the trigger pin to HIGH state for 10 microseconds
   digitalWrite(triggerPin, HIGH);
   delayMicroseconds(10);
@@ -19,17 +21,17 @@ long readUltrasonicDistance(int triggerPin, int echoPin)
   return pulseIn(echoPin, HIGH);
 }
 
-void setup()
+void setup() //시작할때 한 번만 실행시키는 함수
 {
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  Serial.begin(9600); //9600bps로 아두이노가 통신을 시작하겠다
+  pinMode(13, OUTPUT); 
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 }
 
 void loop()
 {
-  distance = 0.01723 * readUltrasonicDistance(7, 6);
+  distance = 0.01723 * readUltrasonicDistance(7, 6); 거리함수는 
   Serial.println(distance);
   if (distance >= 200) {
     digitalWrite(13, HIGH);
